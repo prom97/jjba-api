aaffb5ea62919218c4d66a2775784304277d781e
Pablo <romero.pablo.wd@gmail.com>
2024-06-15 03:17:54 -0300

    content added to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Quotes;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Quotes::create([
+            'character' => 'Wamuu',
+            'desc' => 'Una vida brillante, tan efímera como la burbuja que guarda su legado.',
+        ]);
+        Quotes::create([
+            'character' => 'Stroheim',
+            'desc' => 'El valor no consiste en no tener miedo al ponerte en peligro, sino en hacerlo conscientemente por una causa justa.',
+        ]);
+        Quotes::create([
+            'character' => 'Dio Brando',
+            'desc' => 'La sociedad succiona el almo de un humano. Es de esta manera que volveré a JoJo un cobarde. Entonces podré tomar todo, ¡cuando crezcamos todo será mio!.',
+        ]);
+        Quotes::create([
+            'character' => 'Weather Report',
+            'desc' => 'El tipo de mal que no se da cuenta de que es malo... es el peor que hay.',
+        ]);
+        Quotes::create([
+            'character' => 'Kars',
+            'desc' => 'No soy como Wham, no tengo su romanticismo de guerrero, haré lo que sea necesario. Lo único que importa... ¡¡Es ganar!!.',
+        ]);
+        Quotes::create([
+            'character' => 'Jotaro Kujo',
+            'desc' => 'Yare Yare Daze.',
+        ]);
+        Quotes::create([
+            'character' => 'Rohan Kishibe',
+            'desc' => '¡A mí, Rohan Kishibe, lo que más me gusta es decirle no en la cara a la gente que se cree más poderosa!.',
         ]);
     }
 }
